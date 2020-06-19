86ef1de6535d5b17ee6781fb3a1e8adf1e0e701f
Hamburger <chrischenghc@gmail.com>
2020-06-19 16:53:40 +0800

    Initial Commit

---
+++ FortuneTelling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FortuneTelling
+//
+//  Created by Hamburger on 2020/6/19.
+//  Copyright © 2020 Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
